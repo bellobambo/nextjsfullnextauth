c21cfeceb953ebe0f3f744534b4a59f0e263027b
bellobambo <bellobambo21@gmail.com>
2024-04-07 01:07:57 +0100

    nextauth init

---
+++ src/app/components/Appbar.tsx
@@ -0,0 +1,30 @@
+import { Button, Navbar, NavbarContent, NavbarItem } from '@nextui-org/react'
+import Link from 'next/link'
+import React from 'react'
+
+const Appbar = () => {
+    return (
+        <Navbar>
+
+
+
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <NavbarItem>
+                    <Link color="foreground" href="/home">
+                        Home
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent justify="end">
+        
+                <NavbarItem>
+                    <Button as={Link} color="primary" href="/auth/signup" variant="flat">
+                        Sign Up
+                    </Button>
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar>
+    )
+}
+
+export default Appbar
