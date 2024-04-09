be149019f52af6ef19fde1a14244c8f63a2fa391
bellobambo <bellobambo21@gmail.com>
2024-04-09 13:42:07 +0100

    jwt

---
+++ src/app/auth/activation/[id]/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    params: {
+        id: string;
+    }
+}
+
+const ActivationPage = ({ params }: Props) => {
+    return (
+        <div>{params.id}</div>
+    )
+}
+
+export default ActivationPage
--- src/lib/actions/authActions.ts
+++ src/lib/actions/authActions.ts
@@ -3,16 +3,28 @@
 import prisma from "@/prisma";
 import { User } from "@prisma/client";
 import * as bcrypt from "bcrypt";
+import { compileActivationTemplate, sendMail } from "../mail";
+import { signJWT } from "../jwt";
 
 
 
-export async function registerUser(user : Omit<User, 'id' | 'emailVerified' | 'image'>){
+export async function registerUser(user: Omit<User, 'id' | 'emailVerified' | 'image'>) {
 
     const result = await prisma.user.create({
-        data : {
+        data: {
             ...user,
-            password : await bcrypt.hash(user.password, 10),
+            password: await bcrypt.hash(user.password, 10),
         }
+    });
+
+    const jwtUserId = signJWT({
+        id: result.id,
+        email: result.email,
     })
+    const activationUrl = `${process.env.NEXTAUTH_URL}/auth/activation/${jwtUserId}`
+
+    const body = compileActivationTemplate(user.firstName, activationUrl)
+    await sendMail({ to: user.email, subject: 'Activate Your Account', body })
+    return result;
 
 }
